dce1e421e70d203a189462fb02a08f86305a6e3a
josemanuel1992 <jose_93_87@hotmail.com>
2018-04-03 19:05:05 -0500

    MaquetadoTSGarza
    Maquetado

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+	<meta charset="UTF-8">
+	<title>TS & Garza</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="topHeader">
+			<div class="login float-right">
+				<a href="#">Iniciar Sesión</a>
+			</div>
+		</div>
+		<div class="bottomHeader">
+			<div class="logo float-left">
+				<img src="img/logo.png" alt="TS & Garza Logo">
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#">Inicio</a></li>
+					<li><a href="#">Nosotros</a></li>
+					<li class="activo"><a href="#">Servicios</a></li>
+					<li><a href="#">Contacto</a></li>
+					<li><a href="#">Noticias</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div id="slider">
+			<img src="" alt="">
+		</div>
+	</header>
+	<div class="clearfix"></div>
+	<section id="contenedorNosotros">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-9">
+					<h2 class="text-justify">Nosotros</h2>
+					<h1 class="text-justify">CREAMOS<br/>Soluciones</h1>
+					<h3 class="text-justify">a tus necesidades</h3>
+					<p class="text-justify">El exito de un negocio es dar un excelente servicio o producto a todos nuestros clientes. Toda empresa con exito va de la mano con una Asesoria Contable, Fiscal y Financiera; es por eso que sometemos a su consideración nuestra propuesta de servicios proponiendo el mejor esquema para atender a sus necesidades buscando rentabilidad, legalidad y pofesionalismo</p>
+					<p class="text-justify">Nuestro equipo de trabajo reune el conocimiento y la experiencia de varios años de trabajo en diversas empresas que nos han permitido unir esfuerzos y desarrollar los esquemas de control contable, fiscal y legal que haran eficiente a su empresa.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="contenedorHistorial">
+		<div class="container">
+			<ul class="nav justify-content-center">
+  				<li class="nav-item">
+    				<a class="nav-link active" href="#"><h2 class="text-center">5</h2><br/><p>Sucursales</p></a>
+  				</li>
+  				<li class="nav-item">
+    				<a class="nav-link" href="#"><h2 class="text-center">10</h2><br/><p>Años de experiencia</p></a>
+  				</li>
+  				<li class="nav-item">
+    				<a class="nav-link" href="#"><h2 class="text-center">1,080</h2><br/><p>Clientes Satisfechos</p></a>
+  				</li>
+  				<li class="nav-item">
+    				<a class="nav-link active" href="#"><h2 class="text-center">253</h2><br/><p>Empleados de TS</p></a>
+  				</li>
+			</ul>
+		</div>
+	</section>
+
+
+	<section id="servicios">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 ">
+					
+				</div>
+			</div>
+			
+		</div>
+
+	</section>
+
+
+
+
+	<section id="contenedorImagenes">
+		<div class="row">
+  			<div class="col-sm-4 col-md-6">
+    			<a href="#" class="thumbnail">
+      			<img src="img/img1.jpg" alt="img1" class="img-thumbnail">
+    			</a>
+  			</div>
+  				<div class="col-sm-4 col-md-6">
+    				<a href="#" class="thumbnail">
+      				<img src="img/img2.jpg" alt="img2" alt class="img-responsive"" class="img-thumbnail">
+    				</a>
+				</div>
+					<div class="col-sm-4 col-md-6">
+    					<a href="#" class="thumbnail">
+      					<img src="img/img3.jpg" alt="img3" class="img-thumbnail">
+    					</a>
+					</div>
+					<br/>
+						<div class="col-sm-4 col-md-6">
+    						<a href="#" class="thumbnail">
+      						<img src="img/img4.jpg" alt="img4" class="img-thumbnail">
+    						</a>
+						</div>
+							<div class="col-sm-4 col-md-6">
+    							<a href="#" class="thumbnail">
+      							<img src="img/img5.jpg" alt="img5" class="img-thumbnail">
+    							</a>
+							</div>
+								<div class="col-sm-4 col-md-6">
+    								<a href="#" class="thumbnail">
+      								<img src="img/img6.jpg" alt="img6" class="img-thumbnail">
+    								</a>
+								</div>
+	</section>
+
+	<section id="aplicasion">
+		<div class="container">
+			<div class="row">
+				
+			</div>
+			
+		</div>
+	</section>
+
+    <section id="contactanos">
+	<div class="bottomHeader2">
+			<div class="row">
+                <div class="col-md-12">
+                    <hr>
+                    <div class="row text-center">
+                        <div class="col-sm-2 col-sm-offset-1 col-xs-12">
+                            <h4>CANCÚN</h4>
+                            <p>
+                                <a href="tel: (998) 2063 254">(998) 2063 254</a>
+                                <br>
+                                <a href="tel: (998) 2063 256">(998) 2063 256</a>
+                            </p>
+                        </div>
+                        <div class="col-sm-2 col-xs-12">
+                            <h4>TIJUANA</h4>
+                            <p><a href="tel: (664) 6158 085">(664) 6158 085</a></p>
+                        </div>
+                        <div class="col-sm-2 col-xs-12">
+                            <h4>GUADALAJARA</h4>
+                            <p><a href="tel: (33) 31650 943">(33) 31650 943</a></p>
+                        </div>
+                        <div class="col-sm-2 col-xs-12">
+                            <h4>MONTERREY</h4>
+                            <p><a href="tel: (81) 1853 6145">(81) 1853 6145</a></p>
+                        </div>
+                        <div class="col-sm-2 col-xs-12">
+                            <h4>CD. DE MÉXICO</h4>
+                            <p>
+                                <a href="tel: (55) 7589 4007">(55) 7589 4007</a>
+                                <br>
+                                <a href="tel: (55) 7589 4006">(55) 7589 4006</a>
+                            </p>
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+		</div>
+        </section>
+
+<div class="contactanos">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 col-sm-offset-1">
+                    <h4>CONTÁCTANOS</h4>
+                                        <form name="form" method="post" action="/contacto">
+                    <div class="form-group"><label class="control-label required" for="form_name">Nombre completo</label><input type="text" id="form_name" name="form[name]" required="required" class="form-control" /></div><div class="form-group"><label class="control-label required" for="form_telephone">Número telefónico</label><input type="text" id="form_telephone" name="form[telephone]" required="required" class="form-control" /></div><div class="form-group"><label class="control-label required" for="form_email">Correo electrónico</label><input type="email" id="form_email" name="form[email]" required="required" class="form-control" /></div><div class="form-group"><label class="control-label required" for="form_message">Comentario</label><textarea id="form_message" name="form[message]" required="required" class="form-control"></textarea></div><div class="form-group"><button type="submit" id="form_save" name="form[save]" class="btn-ts btn">Enviar</button></div><input type="hidden" id="form__token" name="form[_token]" value="tTkl6VYzLRV5Cg7gneXeeffr65xwPRZOy3P9lLmOn5U" />
+   
+                </div>
+                <div class="col-sm-6 col-xs-12 pull-right" style="min-height: 400px">
+                    <div class="contactanos__offices text-right">
+                        <p><strong>OFICINAS CORPORATIVAS</strong></p>
+                        <p>
+                            Calle Chilam Balam 261<br>
+                            Sección A1, SM-50, MZ-11, L21<br>
+                            Fracc. Riviera<br>
+                            C.P. 77533
+                        </p>
+                        <p>info@tsdelagarza.com</p>
+                        <p>(998) 206-3256</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="footer__top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5">
+                        <img src="img/logo.png" alt="TS & Garza Logo">
+                        <ul class="list-unstyled">
+                            <li>TS y de la Garza S de R.L. de C.V.</li>
+                            <li>Soluciones Contables y Fiscales Monterrey</li>
+                            <li>Soluciones Contables y Fiscales Guadalajara</li>
+                            <li>Soluciones Contables y Fiscales Tijuana</li>
+                            <li>Soluciones Contables y Fiscales Ciudad de México</li>
+                            <li>Soluciones Contables y Fiscales Cancún</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2 col-md-offset-1 col-xs-6">
+                        <h4>REDES SOCIALES</h4>
+                        
+                    </div>
+                    <div class="col-md-3 col-md-offset-1 col-xs-6">
+                        <h4>SERVICIOS</h4>
+                        <ul class="footer__services"
+                            style="list-style-image: url(/images/svgs/Ts-icon.svg); padding: 0">
+                                                            <li>
+                                    <a href="/servicios-fiscales-contables">Registros Contables y Fiscales</a>
+                                </li>
+                                                            <li>
+                                    <a href="/servicios-asesoria-fiscal">Asesoría Fiscal</a>
+                                </li>
+                                                            <li>
+                                    <a href="/servicios-administracion-de-nomina">Administración de Nómina</a>
+                                </li>
+                                                            <li>
+                                    <a href="/servicios-reclutamiento-de-personal">Selección y Reclutamiento de Personal</a>
+                                </li>
+                                                            <li>
+                                    <a href="/servicios-estudios-socioeconomicos">Estudios Socioeconómicos</a>
+                                </li>
+                                                            <li>
+                                    <a href="/servicios-gestorias">Gestoría</a>
+                                </li>
+                                                            <li>
+                                    <a href="/servicios-registro-de-marca">Registro de Marca</a>
+                                </li>
+                                                    </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer__bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <p>Copyright - TS & DE LA GARZA Consultores</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <script type="text/javascript" src="/js/odometer.min.js"></script>
+        <script src="/build/manifest.js" type="text/javascript"></script>
+    <script src="/build/js/vendor.js" type="text/javascript"></script>
+    <script src="/build/js/main.js" type="text/javascript"></script>
+
+    <script type="text/javascript" src="/js/jquery.YTPlayer.min.js"></script>
+
+    <script type="text/javascript">
+      $("#bgndVideo").YTPlayer();
+    </script>
+</body>
+
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
